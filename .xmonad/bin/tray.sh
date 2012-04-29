348e6baf06694c0354413054fe2d8c2035001008
Bin Jin <bjin@ctrl-d.org>
2012-04-29 19:14:48 +0800

    use percent to set the width of trayer

--- .xmonad/bin/tray.sh
+++ .xmonad/bin/tray.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# set the width of trayer to 64
-# the screen width is 1280, left 95% is xmobar
-
-trayer --edge top --align right --margin 0 --width 64 --widthtype pixel --height 12 \
+trayer --edge top --align right --margin 0 --width 5 --widthtype percent --height 12 \
         --padding 1 --tint 0x000000 --transparent true &
 

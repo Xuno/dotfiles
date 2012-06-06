e5e88a5d9dcd88a7ed9993e48ba3a064cd6bd708
Bin Jin <bjin@ctrl-d.org>
2012-06-07 01:12:57 +0800

    use .profile to store PATH

---
+++ .profile
@@ -0,0 +1,4 @@
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/games
+export PATH=/usr/local/sbin:/usr/local/bin:$PATH
+export PATH=$HOME/bin:$HOME/.cabal/bin:$PATH
+export PATH=/opt/java/bin:/opt/android-sdk/platform-tools:/usr/share/java/apache-ant/bin:$PATH
--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -1,9 +1,6 @@
 #!/usr/bin/env zsh
 
-export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/games
-export PATH=/usr/local/sbin:/usr/local/bin:$PATH
-export PATH=$HOME/bin:$HOME/.cabal/bin:$PATH
-export PATH=/opt/java/bin:/opt/android-sdk/platform-tools:/usr/share/java/apache-ant/bin:$PATH
+source $HOME/.profile
 
 export EDITOR="vim"
 export BROWSER="firefox"

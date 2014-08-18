1b4354da117db9856ca34a0a4978203fea7852f6
Bin Jin <bjin1990@gmail.com>
2014-08-18 16:28:55 +0200

    mpv: update f3kdb deband

--- .mpv/deband.py
+++ .mpv/deband.py
@@ -1,5 +1,5 @@
 import vapoursynth as vs
 core = vs.get_core()
 clip = core.std.Trim(video_in, first=0, length=5000000)
-video_out = core.f3kdb.Deband(clip, output_depth=16)
+video_out = core.f3kdb.Deband(clip, grainy=0, grainc=0, output_depth=16)
 video_out.set_output()

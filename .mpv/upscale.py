e17f9ad3426e3b5dbe9c843a85e9061b7a2d4a63
Bin Jin <bjin1990@gmail.com>
2014-08-19 00:52:58 +0200

    mpv: update vapoursynth filters

--- .mpv/upscale.py
+++
@@ -1,9 +0,0 @@
-import vapoursynth as vs
-core = vs.get_core()
-w = video_in.width
-h = video_in.height
-#video_out = core.fmtcwrap.Scale(video_in, 2560, 1440, kernel="spline36")
-video_out = core.fmtcwrap.Scale(video_in, w * 2, h * 2, kernel="spline36")
-#video_out = core.fmtcwrap.Scale(video_in, w * 2, h * 2, kernel="spline64")
-#video_out = core.std.Transpose(core.eedi3.eedi3(core.std.Transpose(core.eedi3.eedi3(video_in, field=1, dh=1)), field=1, dh=1))
-video_out.set_output()

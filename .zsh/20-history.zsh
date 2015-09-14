171cb6e1b364190de3c6fb97e3fb39067e0dbcb1
Bin Jin <bjin1990@gmail.com>
2015-09-15 00:37:48 +0200

    zsh: add missing keybind for history search

--- .zsh/20-history.zsh
+++ .zsh/20-history.zsh
@@ -6,5 +6,10 @@ export SAVEHIST=$HISTSIZE
 setopt HIST_FIND_NO_DUPS
 setopt HIST_IGNORE_ALL_DUPS
 
+for keycode in '[' '0'; do
+    bindkey "^[${keycode}A" history-substring-search-up
+    bindkey "^[${keycode}B" history-substring-search-down
+done
+unset keycode
 bindkey -M vicmd 'k' history-substring-search-up
 bindkey -M vicmd 'j' history-substring-search-down
